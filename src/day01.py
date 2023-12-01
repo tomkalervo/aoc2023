8a8300559aeb460c82462a60d8041e0130e6cca0
Replit user <>
2023-12-01 08:43:08 +0000

    Day 1, my first python program!

--- src/day01.py
+++ src/day01.py
@@ -1,18 +1,77 @@
 from myModules.inputParser import parseWithFunction  #type: ignore
 
 def _stringParsing(str):
-  e = []
-  for y in str.strip("()").split(","):
-    e.append(int(y))
-  return e
+  # print(str)
+  a = _getIntFromStart(str)
+  b = _getIntFromEnd(str)
+  a += b
+  # print(a)
+  return int(a)
 
+def _getNumberFromString(func):
+  numbers = [
+    "zero", "one", "two", 
+    "three", "four", "five",
+    "six", "seven", "eight", 
+    "nine"
+  ]
+  i = 0
+  for number in numbers:
+    if func(number):
+      break
+    else:
+      i += 1
+  if i < 10:
+    return "{}".format(i)
+  else:
+    return ""
+  
+# Used for part 1
+def _getInts(str, number):
+  for element in str:
+    if element.isdigit():
+      number += element
+      break
+  return number
+  
+# Used for part 2
+def _getIntFromStart(str):
+  number = ""
+  while str:
+    if str[0].isdigit():
+      number = str[0]
+      break
+      
+    else:
+      number = _getNumberFromString(str.startswith)
+      if number:
+        break
+      else:
+        str = str[1:]
+    
+  return number
+
+def _getIntFromEnd(str):
+  number = ""
+  while str:
+    if str[-1].isdigit():
+      number = str[-1]
+      break
+  
+    else:
+      number = _getNumberFromString(str.endswith)
+      if number:
+        break
+      else:
+        str = str[:-1]
+  
+  return number
 
 def _listOps(ls):
-  for e in ls:
-    print("{} + {} = {}".format(e[0], e[1], e[0] + e[1]))
+  return sum(ls)
 
 
 if __name__ == "__main__":
   func = _stringParsing
   parsed_input = parseWithFunction(func)
-  _listOps(parsed_input)
+  print(_listOps(parsed_input))
